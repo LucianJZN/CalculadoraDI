e0222275f8e618c5275a43d58c4226ac99b6110b
Lucian Stelian <lucianstelian.grecianu@elorrieta-errekamari.com>
2024-09-30 20:27:54 +0200

    Vista horizontal terminada.

--- app/src/main/java/com/example/calculadorajp/MainActivity.kt
+++ app/src/main/java/com/example/calculadorajp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculadorajp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.ComponentActivity
@@ -33,8 +34,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.calculadorajp.ui.theme.CalculadoraJPTheme
@@ -44,17 +47,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            vista()
+        }
+    }
+}
+
+@Composable
+fun vista(){
+    val configuration = LocalConfiguration.current
+
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            // Diseño para orientación Landscape
+            VistaGeneralHorizontal()
+        }
+        Configuration.ORIENTATION_PORTRAIT -> {
+            // Diseño para orientación Portrait
             VistaGeneral()
         }
     }
 }
 
 @Composable
-fun Resultado(resultado: String) {
+fun Resultado(resultado: String, size: Dp) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .size(200.dp)
+            .size(size)
             .background(Color.White, shape = RoundedCornerShape(12.dp))
             .border(2.dp, Color.DarkGray, RoundedCornerShape(12.dp)) // Aplicar borde
     ) {
@@ -86,6 +105,23 @@ fun Boton(name: String, onClick: () -> Unit) {
     }
 }
 @Composable
+fun BotonH(name: String, onClick: () -> Unit) {
+    TextButton(
+        onClick  = onClick,
+        modifier = Modifier
+            .width(120.dp)  // Ancho del botón
+            .height(60.dp)  // Alto del botón
+            .border(2.dp, Color.DarkGray, RoundedCornerShape(12.dp)) // Aplicar borde
+            .background(Color.Gray, shape = RoundedCornerShape(12.dp))
+    ) {
+        Text(
+            text = name,
+            fontSize = 40.sp, // Tamaño de la fuente
+            color = Color.White // Color del texto (opcional)
+        )
+    }
+}
+@Composable
 fun BotonSpecial(name: String, onClick: () -> Unit) {
     TextButton(
         onClick  = onClick,
@@ -102,13 +138,30 @@ fun BotonSpecial(name: String, onClick: () -> Unit) {
         )
     }
 }
+@Composable
+fun BotonSpecialH(name: String, onClick: () -> Unit) {
+    TextButton(
+        onClick  = onClick,
+        modifier = Modifier
+            .width(120.dp)  // Ancho del botón
+            .height(60.dp)  // Alto del botón
+            .border(2.dp, Color.DarkGray, RoundedCornerShape(12.dp)) // Aplicar borde
+            .background(Color(0xFF4B4B4B), shape = RoundedCornerShape(12.dp))
+    ) {
+        Text(
+            text = name,
+            fontSize = 40.sp, // Tamaño de la fuente
+            color = Color.White // Color del texto (opcional)
+        )
+    }
+}
 
 @Composable
 fun VistaGeneral() {var resultado by remember { mutableStateOf("") }
     Column(modifier = Modifier.padding(0.dp,10.dp,0.dp,0.dp)
         .fillMaxSize()
         .padding(10.dp), verticalArrangement = Arrangement.SpaceEvenly) {
-        Resultado(resultado = resultado)
+        Resultado(resultado = resultado, 200.dp)
         Row(modifier = Modifier.padding(0.dp,15.dp,0.dp,0.dp).fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
             Boton(name = "7", onClick = {resultado+="7"}) //{ resultado.value += "1" }
             Boton(name = "8", onClick = {resultado+="8"})
@@ -140,9 +193,43 @@ fun VistaGeneral() {var resultado by remember { mutableStateOf("") }
     }
 }
 
+@Composable
+fun VistaGeneralHorizontal() {var resultado by remember { mutableStateOf("") }
+    Column(modifier = Modifier.padding(0.dp,0.dp,0.dp,0.dp)
+        .fillMaxSize()
+        .padding(10.dp), verticalArrangement = Arrangement.SpaceEvenly) {
+        Resultado(resultado = resultado, 50.dp)
+        Row(modifier = Modifier.padding(0.dp,0.dp,0.dp,0.dp).fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+            BotonH(name = "7", onClick = {resultado+="7"}) //{ resultado.value += "1" }
+            BotonH(name = "8", onClick = {resultado+="8"})
+            BotonH(name = "9", onClick = {resultado+="9"})
+            BotonH(name = "0", onClick = {resultado+="0"})
+        }
+        Row(modifier = Modifier.padding(0.dp,0.dp,0.dp,0.dp).fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+            BotonH(name = "4", onClick = {resultado+="4"})
+            BotonH(name = "5", onClick = {resultado+="5"})
+            BotonH(name = "6", onClick = {resultado+="6"})
+            BotonSpecialH(name = "C", onClick = {
+            })
+        }
+        Row(modifier = Modifier.padding(0.dp,0.dp,0.dp,0.dp).fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+            BotonH(name = "1", onClick = {resultado+="1"})
+            BotonH(name = "2", onClick = {resultado+="2"})
+            BotonH(name = "3", onClick = {resultado+="3"})
+            BotonSpecialH(name = "*", onClick = {resultado+="*"})
+        }
+        Row(modifier = Modifier.padding(0.dp,0.dp,0.dp,0.dp).fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+            BotonSpecialH(name = "-", onClick = {resultado+="-"})
+            BotonSpecialH(name = "+", onClick = {resultado+="+"})
+            BotonSpecialH(name = "/", onClick = {resultado+="/"})
+            BotonSpecialH(name = "=", onClick = {resultado+="="})
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun Preview(){
-    VistaGeneral()
+    vista()
 }
 
